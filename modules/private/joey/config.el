f2ed05fc7b2d585494d5c8e65003f98e9e54fab3
Liu, Joey <joey.liu@philips.com>
2018-05-05 22:33:16 -0400

    Clean: testing functions

--- modules/private/joey/config.el
+++ modules/private/joey/config.el
@@ -1069,32 +1069,3 @@ so change the default 'F' binding in the agenda to allow both"
                          (org-tags-match-list-sublevels nil))))
                  nil)))))
 
-
-(defun +python|setup-shell ()
-  (interactive)
-  (when (executable-find "ipython")
-    (setq python-shell-interpreter "ipython"
-          python-shell-interpreter-args "-i --simple-prompt --no-color-info"
-          python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-          python-shell-prompt-block-regexp "\\.\\.\\.\\.: "
-          python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-          python-shell-completion-setup-code
-          "from IPython.core.completerlib import module_completion"
-          python-shell-completion-string-code
-          "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")))
-
-(defun +python|detect-pyenv-version ()
-  "Detect the pyenv version for the current project and set the relevant
-environment variables."
-  (interactive)
-  (when-let* ((version-str (shell-command-to-string "python --version 2>&1 | cut -d' ' -f2")))
-    (setq version-str (string-trim version-str)
-          +python-current-version version-str)
-    (let ((pyenv-current-path (concat +python-pyenv-root "/versions/" version-str)))
-      (when (file-directory-p pyenv-current-path)
-        (setq pythonic-environment pyenv-current-path)))
-    (when (member version-str +python-pyenv-versions)
-      (setenv "PYENV_VERSION" version-str))))
-
-
-
