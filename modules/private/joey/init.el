cc6a0b21fda2825c20e19d2d9e9bf67c988cc38c
Liu, Joey <joey.liu@philips.com>
2018-12-04 15:20:15 -0500

    disable display-line-numbers-type setting

--- modules/private/joey/init.el
+++ modules/private/joey/init.el
@@ -1,3 +1,4 @@
 (setq doom-font (font-spec :family "Fira Code Retina" :size 12)
       ;; doom-unicode-font (font-spec :family "Material Icons" :size 12)
-      display-line-numbers-type 'visual)
+      ;; display-line-numbers-type 'visual
+      )
