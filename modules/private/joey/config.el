5de201b0c5584b856e4fd9af4b0d0be7cd5b7d74
Liu, Joey <joey.liu@philips.com>
2018-12-04 15:19:48 -0500

    remove pretty magit settings

--- modules/private/joey/config.el
+++ modules/private/joey/config.el
@@ -33,66 +33,7 @@
 
 ;; pretty magit
 (after! magit
-  (require 'dash)
-
-  (setq magit-revision-show-gravatars nil)
-
-  (defmacro pretty-magit (WORD ICON PROPS &optional NO-PROMPT?)
-    "Replace sanitized WORD with ICON, PROPS and by default add to prompts."
-    `(prog1
-         (add-to-list 'pretty-magit-alist
-                      (list (rx bow (group ,WORD (eval (if ,NO-PROMPT? "" ":"))))
-                            ,ICON ',PROPS))
-       (unless ,NO-PROMPT?
-         (add-to-list 'pretty-magit-prompt (concat ,WORD ": ")))))
-
-  (setq pretty-magit-alist nil)
-  (setq pretty-magit-prompt nil)
-  (pretty-magit "Feature"  ? (:family "github-octicons" :foreground "slate gray" :height 1.2))
-  (pretty-magit "Add"      ? (:family "github-octicons" :foreground "#375E97" :height 1.2))
-  (pretty-magit "Fix"      ? (:family "github-octicons" :foreground "#FB6542" :height 1.2))
-  (pretty-magit "Clean"    ? (:family "FontAwesome" :foreground "#FFBB00" :height 1.2))
-  (pretty-magit "Docs"     ? (:family "github-octicons" :foreground "#3F681C" :height 1.2))
-  (pretty-magit "master"   ? (:family "all-the-icons" :box t :height 1.2) t)
-  (pretty-magit "upstream" ? (:family "github-octicons" :box t :height 1.2) t)
-  (pretty-magit "origin"   ? (:family "github-octicons" :box t :height 1.2) t)
-
-  (defun add-magit-faces ()
-    "Add face properties and compose symbols for buffer from pretty-magit."
-    (interactive)
-    (with-silent-modifications
-      (--each pretty-magit-alist
-        (-let (((rgx icon props) it))
-          (save-excursion
-            (goto-char (point-min))
-            (while (search-forward-regexp rgx nil t)
-              (compose-region
-               (match-beginning 1) (match-end 1) icon)
-              (when props
-                (add-face-text-property
-                 (match-beginning 1) (match-end 1) props))))))))
-
-  (advice-add 'magit-status :after 'add-magit-faces)
-  (advice-add 'magit-refresh-buffer :after 'add-magit-faces)
-
-  ;; ivy helper for pretty magit
-  (setq use-magit-commit-prompt-p nil)
-  (defun use-magit-commit-prompt (&rest args)
-    (setq use-magit-commit-prompt-p t))
-
-  (defun magit-commit-prompt ()
-    "Magit prompt and insert commit header with faces."
-    (interactive)
-    (when use-magit-commit-prompt-p
-      (setq use-magit-commit-prompt-p nil)
-      (insert (ivy-read "Commit Type " pretty-magit-prompt
-                        :require-match t :sort t :preselect "Add: "))
-      (evil-insert 1)  ; If you use evil
-      ))
-
-  (remove-hook 'git-commit-setup-hook 'with-editor-usage-message)
-  (add-hook 'git-commit-setup-hook 'magit-commit-prompt)
-  (advice-add 'magit-commit :after 'use-magit-commit-prompt))
+  (setq magit-revision-show-gravatars nil))
 
 ;; org
 (after! org
